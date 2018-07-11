b535d0690dc80061e717fb0985e677275d180841
孙培伦 <1263685152@qq.con>
2018-07-11 16:17:53 +0800

    建立了service和controller的关系，添加了视频简介页面ftl

---
+++ src/main/java/com/oahzuw/controller/VideoIntroController.java
@@ -0,0 +1,37 @@
+package com.oahzuw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * @author 孙培伦 (sunobject@foxmail.com)
+ * @date 2018/7/10 17:26
+ * 用于视频简介模板
+ */
+@Controller
+public class VideoIntroController {
+
+    
+
+    /**
+     * 连接视频简介模板，ajax从数据库动态获取到视频详情
+     * @author（孙培伦）
+     * @date （2018/7/10 17:27）
+     */
+    @RequestMapping("/intro.action")
+    public void Intro(){
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/oahzuw/service/IntroService.java
@@ -0,0 +1,20 @@
+package com.oahzuw.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 查询视频简介
+ *
+ * @author 孙培伦
+ */
+@Service
+public interface IntroService {
+
+    /**
+     * 查询视频简介
+     * @author 孙培伦
+     * @return
+     */
+
+
+}
